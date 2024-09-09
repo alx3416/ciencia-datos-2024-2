f52c27f8420157c7f2c367c6415eddb7e3239b55
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-09-09 16:31:46 -0600

    updating submodules

---
+++ main.py
@@ -0,0 +1,4 @@
+from utils.imports import read_diabetes
+
+data = read_diabetes("data/diabetes.tab.txt")
+print("ok")
---
+++ utils/imports.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def read_diabetes(path):
+    return pd.read_csv(path)
