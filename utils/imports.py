cd5bf81b4d02e8b9e0c07645ef0bef0f2d113488
alx3416 <63263335+alx3416@users.noreply.github.com>
2024-09-11 15:29:04 -0600

    creating a class to import data

--- utils/imports.py
+++ utils/imports.py
@@ -1,4 +1,26 @@
 import pandas as pd
 
-def read_diabetes(path):
-    return pd.read_csv(path)
+
+class Dataset:
+    def __init__(self, path):
+        self.dataframe = []
+        self.columns = []
+        self.rows = []
+
+    def read_data(path):
+        return pd.read_csv(path)
+
+
+class Diabetes(Dataset):
+    def __init__(self, path):
+        self.dataframe = []
+        self.columns = 11
+        self.rows = 442
+
+    def read_data(path):
+        return pd.read_csv(path, sep="/t")
+
+df = Diabetes("data/diabetes.tab.txt")
+
+print(df.rows)
+print(df.columns)
